45db2dcfb80cd87afcf51d71b1d0188fd8fe52b5
Mariasachs <maria.sachs@escola.pr.gov.br>
2024-10-17 11:28:20 -0300

    /INTRODUÇ^

---
+++ introducaoJS/script.js
@@ -0,0 +1,16 @@
+const idade = 15
+const nome = "igor"
+const sobrenome = "guilherme"
+const telefone = 4198989898
+const estaCasado = false
+// crie uma variavel para cada um dos
+// dados listados:(nome, sobrenome, telefone, estaCasado= true ou false)
+// imprima cada um dos valores com console.log(<variavel>)
+
+
+
+console.log(idade)
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estaCasado)
